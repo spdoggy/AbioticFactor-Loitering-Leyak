05dd2404ab0065b289a73eb0cf5ca153679fd5af
spdoggy <131057545+spdoggy@users.noreply.github.com>
2026-09-03 20:46:09 -0400

    Update config_admin.lua for release

--- config_admin.lua
+++ config_admin.lua
@@ -1,6 +1,6 @@
 return {
     -- Specify the Server Admin's Name Here
     -- For use with chat commands
-    admin_name = "SpDoggy",
-    admin_id = "76561197990919352",
+    admin_name = "YourDisplayNameHere",
+    admin_id = "0131234YourSteamIDHere",
 }
